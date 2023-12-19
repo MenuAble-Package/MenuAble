a3620ae7f8647eeac632f9097d697f703411ec53
KamCy Ahaka <kamcyahaka@gmail.com>
2023-12-19 20:28:36 +0100

    feat(menually.ts): complete base feature for tab trap

--- src/MenuAlly.ts
+++ src/MenuAlly.ts
@@ -3,7 +3,7 @@ import handleKeyPress from './utils/handleKeyPress.ts';
 type Elements = {
   menuButton: Element | null;
   menuElement: Element | null;
-  menuItemsElement: NodeListOf<Element> | null;
+  focusableMenuItems: HTMLElement[] | NodeListOf<HTMLElement>;
 };
 
 type MenuAllyInitOptions = {
@@ -32,13 +32,28 @@ class MenuAlly {
   getElements(): Elements {
     const menuButton = document.querySelector(`${this.menuControl}`);
     const menuElement = document.querySelector(`${this.menu}`);
-    const menuItemsElement = document.querySelectorAll(`${this.menu}`);
 
-    return { menuButton, menuElement, menuItemsElement };
+    let focusableMenuItems: HTMLElement[] | NodeListOf<HTMLElement>;
+
+    if (menuElement !== null && this.menuItems === undefined) {
+      const menuChildren = Array.from(menuElement.children);
+
+      focusableMenuItems = menuChildren.filter(child => {
+        return (
+          child instanceof HTMLAnchorElement ||
+          child instanceof HTMLButtonElement ||
+          child instanceof HTMLInputElement
+        );
+      }) as HTMLElement[];
+    } else {
+      focusableMenuItems = document.querySelectorAll(`${this.menuItems}`);
+    }
+
+    return { menuButton, menuElement, focusableMenuItems };
   }
 
   createTabTrap(): void {
-    const { menuButton, menuElement } = this.getElements();
+    const { menuButton, menuElement, focusableMenuItems } = this.getElements();
 
     if (menuButton !== null && menuElement !== null) {
       if (this.initOptions?.ariaExpanded === true) {
@@ -56,23 +71,26 @@ class MenuAlly {
           menuButton.setAttribute('aria-expanded', 'true');
         }
 
-        const lastFocusedElement = document.activeElement;
-        const focusableMenuItems = menuElement.querySelectorAll(
-          `${this.menu} > a, ${this.menu} > button ${this.menu} > input`,
-        );
+        if (focusableMenuItems.length === 0) {
+          throw new Error(
+            'Unable to create tab trap. No focusable element was given and no direct children exist',
+          );
+        } else {
+          const lastFocusedElement = document.activeElement;
 
-        const firstTabStop = focusableMenuItems[0] as HTMLElement;
-        firstTabStop.focus();
+          const firstTabStop = focusableMenuItems[0];
+          firstTabStop.focus();
 
-        focusableMenuItems.forEach(item => {
-          item.addEventListener('keyup', event => {
-            handleKeyPress(
-              event as KeyboardEvent,
-              lastFocusedElement as HTMLElement,
-              focusableMenuItems as NodeListOf<HTMLElement>,
-            );
+          focusableMenuItems.forEach(item => {
+            item.addEventListener('keyup', event => {
+              handleKeyPress(
+                event,
+                lastFocusedElement as HTMLElement,
+                focusableMenuItems as HTMLElement[],
+              );
+            });
           });
-        });
+        }
       });
     } else {
       throw new Error(
